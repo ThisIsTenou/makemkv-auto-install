ef2709cc53644d2a9bc5d1677f51a054c775da02
Leo <57889616+ThisIsTenou@users.noreply.github.com>
2021-08-20 12:54:06 +0200

    Update makemkv-auto-install.sh

--- makemkv-auto-install.sh
+++ makemkv-auto-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $USER != root ]; then
-		echo "This script needs to be executed with sudo!"
-		exit 1
+if [ "$USER" != root ] && [ "$SUDO_USER" != root ]; then
+	echo "This script needs to be executed with sudo!"
+	exit 1
 fi
 read -p 'Please enter the MKV version you would like to install: ' version
 if ! curl -s --head https://www.makemkv.com/download/makemkv-bin-$version.tar.gz | head -n 1 | grep -q 200; then
@@ -10,6 +10,7 @@ if ! curl -s --head https://www.makemkv.com/download/makemkv-bin-$version.tar.gz
 	exit 1
 fi
 prefix="makemkv-$(date +"%s")"
+homedir="$(eval echo ~"${SUDO_USER:-$USER}")"
 mkdir /tmp/$prefix
 apt-get update
 apt-get install -y build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev qtbase5-dev zlib1g-dev nasm libfdk-aac-dev sed wget curl tar setcd
@@ -29,18 +30,18 @@ make install
 cd /tmp/$prefix/makemkv-bin-$version
 make
 sudo make install
-mkdir -p /home/$SUDO_USER/.MakeMKV
-mkdir -p /home/$SUDO_USER/Videos
-touch /home/$SUDO_USER/.MakeMKV/settings.conf
-tee -a /home/$SUDO_USER/.MakeMKV/settings.conf > /dev/null <<EOT
+mkdir -p $homedir/.MakeMKV
+mkdir -p $homedir/Videos
+touch $homedir/.MakeMKV/settings.conf
+tee -a $homedir/.MakeMKV/settings.conf > /dev/null <<EOT
 app_DefaultOutputFileName = "{NAME2}{-:CMNT2}{-:DY}{unknown-title:+DFLT}{_t:AN2}"
 pp_DestinationDir = "$(realpath ~/Videos)"
 app_DestinationType = "3"
 app_ExpertMode = "1"
 app_InterfaceLanguage = "eng"
-echo "app_Key = \"$(curl -s https://www.makemkv.com/forum/viewtopic.php?t=1053 | grep -o -P '(?<=\<code\>).*(?=\<\/code\>)')\""
+echo "app_Key = \"$(curl -sL https://www.makemkv.com/forum/viewtopic.php?t=1053 | grep -o -P '(?<=\<code\>).*(?=\<\/code\>)')\""
 dvd_MinimumTitleLength = "120"
 io_SingleDrive = "1"
 EOT
-chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.MakeMKV/settings.conf
+chown $SUDO_USER:$SUDO_USER $homedir/.MakeMKV/settings.conf
 rm -rf /tmp/$prefix
